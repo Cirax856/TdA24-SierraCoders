398350b684ece4b208edcaa1140d8fa71b19dd9c
unknown <mata.v114@gmail.com>
2023-11-12 22:00:12 +0100

    -Api

--- aspnetapp/Models/Lecturer.cs
+++ aspnetapp/Models/Lecturer.cs
@@ -1,31 +1,34 @@
-﻿namespace aspnetapp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace aspnetapp.Models
 {
     public class Lecturer
     {
-        public Guid UUID { get; set; }
-        public string title_before { get; set; }
-        public string first_name { get; set; }
-        public string middle_name { get; set; }
-        public string last_name { get; set; }
-        public string title_after { get; set; }
-        public string picture_url { get; set; }
-        public string location { get; set; }
-        public string claim { get; set; }
-        public string bio { get; set; }
-        public Tag[] tags { get; set; }
-        public int price_per_hour { get; set; }
+        [StringLength(36, MinimumLength = 36)]
+        public required Guid UUID { get; set; }
+        public string? title_before { get; set; }
+        public required string first_name { get; set; }
+        public string? middle_name { get; set; }
+        public required string last_name { get; set; }
+        public string? title_after { get; set; }
+        public string? picture_url { get; set; }
+        public string? location { get; set; }
+        public string? claim { get; set; }
+        public string? bio { get; set; }
+        public required Tag[] tags { get; set; }
+        public uint? price_per_hour { get; set; }
         public Contact contact { get; set; }
 
         public class Tag
         {
-            public Guid uuid { get; set; }
-            public string name { get; set; }
+            public required Guid uuid { get; set; }
+            public required string name { get; set; }
         }
 
         public class Contact
         {
-            public string[] telephone_numbers { get; set; }
-            public string[] emails { get; set; }
+            public required string[] telephone_numbers { get; set; }
+            public required string[] emails { get; set; }
         }
     }
 }
