8d225a0f6ebed58447d06344475f06f4c92cc80c
unknown <mata.v114@gmail.com>
2023-11-12 22:00:12 +0100

    -Api

--- aspnetapp/Controllers/ApiCntroller.cs
+++ aspnetapp/Controllers/ApiCntroller.cs
@@ -1,15 +1,138 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Reflection;
+using System.Text.Json;
+using aspnetapp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace aspnetapp.Controllers
 {
     [ApiController]
-    public class ApiController : Controller
+    [Route("lecturers")]
+    public class LecturersController : Controller
     {
-        [HttpGet]
-        [Route("api")]
-        public IActionResult Api()
+        [HttpPost]
+        public ActionResult Post()
         {
-            return Json(new Secret());
+            try
+            {
+                string requestText;
+                using (StreamReader reader = new StreamReader(Request.Body))
+                    requestText = reader.ReadToEnd();
+
+                Lecturer lecturer;
+                try
+                {
+                    lecturer = JsonSerializer.Deserialize<Lecturer>(requestText);
+                } catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                    return StatusCode(400);
+                }
+
+                // validate
+                if (lecturer.contact.emails.Length < 1)
+                    return StatusCode(400);
+                else
+                    lecturer.contact.emails = lecturer.contact.emails.Distinct().ToArray();
+
+                if (lecturer.contact.telephone_numbers.Length < 1)
+                    return StatusCode(400);
+                else
+                    lecturer.contact.telephone_numbers = lecturer.contact.telephone_numbers.Distinct().ToArray();
+
+                // TODO add to database
+                throw new NotImplementedException("/lecturers POST isn't implemented");
+
+                return Json(lecturer);
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return StatusCode(500); // Internal server error
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            Lecturer[] lectures;
+            // TODO get lecturers from db (SELECT *)
+
+            // testing only
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            string resourceName = "aspnetapp.lecturer.json";
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                lectures = new Lecturer[]
+                {
+                    JsonSerializer.Deserialize<Lecturer>(reader.ReadToEnd())
+                };
+            }
+
+            return Json(lectures);
+        }
+
+        [HttpGet]
+        [Route("{guid}")]
+        public ActionResult SpecificGet(Guid guid)
+        {
+            Lecturer lecturer;
+            // TODO get lecturer from db
+            throw new NotImplementedException("/lecturers/{guid} GET isn't implemented");
+
+            return Json(lecturer);
+        }
+
+        [HttpPut]
+        [Route("{guid}")]
+        public ActionResult Put(Guid guid)
+        {
+            try
+            {
+                string requestText;
+                using (StreamReader reader = new StreamReader(Request.Body))
+                    requestText = reader.ReadToEnd();
+
+                Lecturer lecturer;
+                try
+                {
+                    lecturer = JsonSerializer.Deserialize<Lecturer>(requestText);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                    return StatusCode(400);
+                }
+
+                // validate
+                if (lecturer.contact.emails.Length < 1)
+                    return StatusCode(400);
+                else
+                    lecturer.contact.emails = lecturer.contact.emails.Distinct().ToArray();
+
+                if (lecturer.contact.telephone_numbers.Length < 1)
+                    return StatusCode(400);
+                else
+                    lecturer.contact.telephone_numbers = lecturer.contact.telephone_numbers.Distinct().ToArray();
+
+                // TODO get lecturer from db, is null return StatusCode(404), else put new data to db
+                throw new NotImplementedException("/lecturers/{guid} PUT isn't implemented");
+
+                return Json(lecturer);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return StatusCode(500); // Internal server error
+            }
+        }
+
+        [HttpDelete]
+        [Route("{guid}")]
+        public ActionResult Delete(Guid guid)
+        {
+            // TODO if lecturer with guid exists, delete, StatusCode(204), else StatusCode(404)
+            throw new NotImplementedException("/lecturers/{guid} DELETE isn't implemented");
         }
     }
 
---
+++ aspnetapp/Models/LecturerRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace aspnetapp.Models
+{
+    public class LecturerRequest
+    {
+        [StringLength(36, MinimumLength = 36)]
+        public Guid uuid { get; set; }
+        public string name { get; set; }
+    }
+}
