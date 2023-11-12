a013225d2dc04c67a4632ea5e6cd1d3b4783204f
unknown <mata.v114@gmail.com>
2023-11-12 22:17:35 +0100

    Refactoring

--- aspnetapp/Models/Lecturer.cs
+++ aspnetapp/Models/Lecturer.cs
@@ -5,7 +5,7 @@ namespace aspnetapp.Models
     public class Lecturer
     {
         [StringLength(36, MinimumLength = 36)]
-        public required Guid UUID { get; set; }
+        public Guid UUID { get; set; }
         public string? title_before { get; set; }
         public required string first_name { get; set; }
         public string? middle_name { get; set; }
@@ -15,9 +15,27 @@ namespace aspnetapp.Models
         public string? location { get; set; }
         public string? claim { get; set; }
         public string? bio { get; set; }
-        public required Tag[] tags { get; set; }
+        public Tag[] tags { get; set; }
         public uint? price_per_hour { get; set; }
         public Contact contact { get; set; }
+
+        public static bool IsValid(Lecturer lecturer)
+        {
+            if (lecturer.tags == null)
+                lecturer.tags = new Tag[0];
+
+            if (lecturer.contact.emails.Length < 1)
+                return false;
+            else
+                lecturer.contact.emails = lecturer.contact.emails.Distinct().ToArray();
+
+            if (lecturer.contact.telephone_numbers.Length < 1)
+                return false;
+            else
+                lecturer.contact.telephone_numbers = lecturer.contact.telephone_numbers.Distinct().ToArray();
+
+            return true;
+        }
 
         public class Tag
         {
