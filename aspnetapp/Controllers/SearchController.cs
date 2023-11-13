f668ac5a2aa3b462c36baec63cf23f46ae8acff4
unknown <mata.v114@gmail.com>
2023-11-13 20:44:08 +0100

    added /search

---
+++ aspnetapp/Controllers/SearchController.cs
@@ -0,0 +1,42 @@
+﻿using aspnetapp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspnetapp.Controllers
+{
+    [ApiController]
+    [Route("search")]
+    public class SearchController : Controller
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            Lecturer[] lecturers;
+            // TODO get lecturers from db
+            throw new NotImplementedException("/search GET isn't implemented");
+
+            if (Request.Query.ContainsKey("name"))
+            {
+                // sort
+                Searcher.RatedString[] ratedLecturers =
+                    Searcher.Search(lecturers, lecturer => lecturer.DisplayName, Request.Query["name"]);
+
+                // remove entries with score lower than or equal to 0
+                int i;
+                for (i = 0; i < ratedLecturers.Length; i++)
+                    if (ratedLecturers[i].Score <= 0f)
+                        break;
+
+                ratedLecturers = ratedLecturers.Take(i).ToArray();
+
+                Lecturer[] _lecturers = new Lecturer[lecturers.Length];
+                Array.Copy(lecturers, _lecturers, lecturers.Length);
+                lecturers = new Lecturer[ratedLecturers.Length];
+                for (i = 0; i < ratedLecturers.Length; i++)
+                    lecturers[i] = _lecturers[ratedLecturers[i].OgIndex];
+            }
+            // TODO add filtering by price (from - to, ui - double slider?), by tags, by location
+
+            return Json(lecturers);
+        }
+    }
+}
---
+++ aspnetapp/Search.cs
@@ -0,0 +1,57 @@
+﻿namespace aspnetapp
+{
+    public static class Searcher
+    {
+        public static RatedString[] Search<T>(T[] array, Func<T, string> getValue, string input)
+        {
+            RatedString[] ratedArray = new RatedString[array.Length];
+            for (int i = 0; i < array.Length; i++)
+                ratedArray[i] = Rate(getValue(array[i]), i, input);
+
+            Array.Sort(ratedArray);
+
+            return ratedArray;
+        }
+
+        public static RatedString Rate(string s, int ogIndex, string input)
+        {
+            float score = 0f;
+            bool found = false;
+            int index = 0;
+
+            for (int i = 0; i < s.Length && index < input.Length; i++)
+            {
+                if (s[i] == input[index])
+                {
+                    score += 2f;
+                    index++;
+                    found = true;
+                } else if (found)
+                {
+                    score--;
+                } else
+                {
+                    score -= 0.5f;
+                }
+            }
+
+            return new RatedString(s, ogIndex, score);
+        }
+
+        public struct RatedString : IComparable<RatedString>
+        {
+            public string Value;
+            public int OgIndex;
+            public float Score;
+
+            public RatedString(string _value, int _ogIndex, float _score)
+            {
+                Value = _value;
+                OgIndex = _ogIndex;
+                Score = _score;
+            }
+
+            public int CompareTo(RatedString other) => Score.CompareTo(other.Score);
+        }
+    }
+}
