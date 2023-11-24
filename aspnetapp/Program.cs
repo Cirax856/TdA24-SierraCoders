5c8aaba50201b4d7378cb616cf1ce1f13ca6347e
Matěj Veselý <59245Vesely@student.sstebrno.cz>
2023-11-24 13:01:47 +0100

    Removed some things

--- aspnetapp/Program.cs
+++ aspnetapp/Program.cs
@@ -18,13 +18,6 @@ namespace aspnetapp {
             builder.Services.AddRazorPages();
             builder.Services.AddHealthChecks();
             builder.Services.AddControllers();
-            //builder.Services.AddDbContext<LecturerContext>(opt => opt.UseInMemoryDatabase("LecturerList"));
-            //builder.Services.AddScoped<LecturerContext>();
-
-            // builder.Services.ConfigureHttpJsonOptions(options =>
-            // {
-            //     options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
-            // });
 
             WebApplication app = builder.Build();
 
@@ -53,27 +46,6 @@ namespace aspnetapp {
             app.Lifetime.ApplicationStopping.Register(() =>
             {
                 cancellation.Cancel();
-            });
-
-            app.MapGet("/Environment", () =>
-            {
-                return new EnvironmentInfo();
-            });
-
-            // This API demonstrates how to use task cancellation
-            // to support graceful container shutdown via SIGTERM.
-            // The method itself is an example and not useful.
-            app.MapGet("/Delay/{value}", async (int value) =>
-            {
-                try
-                {
-                    await Task.Delay(value, cancellation.Token);
-                }
-                catch (TaskCanceledException)
-                {
-                }
-
-                return new Operation(value);
             });
 
             app.Run();
