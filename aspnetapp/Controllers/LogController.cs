b53a869dbf6ff32a8f4ec2a12b43b6bfae5fb442
unknown <mata.v114@gmail.com>
2023-11-27 16:41:12 +0100

    Added Log

---
+++ aspnetapp/Controllers/LogController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace aspnetapp.Controllers
+{
+    [ApiController]
+    public class LogController : Controller
+    {
+        [HttpGet]
+        [Route("log")]
+        public ActionResult GetLog()
+        {
+            return Content(Log.ToString());
+        }
+    }
+}
