568afc39bedc29903f1d8ad48c8c87fea4c5cedb
SuperMatejCZ <mata.v114@gmail.com>
2023-12-08 13:30:13 +0100

    Implemented DB

--- aspnetapp/Database.cs
+++ aspnetapp/Database.cs
@@ -9,8 +9,86 @@ namespace aspnetapp
 {
     public static class Database
     {
-        public static readonly Dictionary<Guid, Lecturer> lectuerers = new Dictionary<Guid, Lecturer>();
+        public static readonly Dictionary<Guid, DbLecturer> lectuerers = new Dictionary<Guid, DbLecturer>();
         public static readonly List<Lecturer.Tag> tags = new List<Lecturer.Tag>();
+
+        public static void Init()
+        {
+            runCommand(command =>
+            {
+                command.CommandText = @"
+                    CREATE TABLE IF NOT EXISTS Lecturers (
+                        UUID TEXT PRIMARY KEY,
+                        title_before TEXT,
+                        first_name TEXT,
+                        middle_name TEXT,
+                        last_name TEXT,
+                        title_after TEXT,
+                        picture_url TEXT,
+                        location TEXT,
+                        claim TEXT,
+                        bio TEXT,
+                        tags TEXT,
+                        price_per_hour INTEGER,
+                        contact TEXT
+                    );
+                ";
+                command.ExecuteNonQuery();
+            });
+            runCommand(command =>
+            {
+                command.CommandText = @"
+                    CREATE TABLE IF NOT EXISTS Tags (
+                        UUID TEXT PRIMARY KEY,
+                        name TEXT
+                    );
+                ";
+                command.ExecuteNonQuery();
+            });
+
+            List<DbLecturer> _lecturers = new List<DbLecturer>();
+            runCommand(command =>
+            {
+                command.CommandText = "SELECT * FROM Lecturers;";
+
+                using (SqliteDataReader reader = command.ExecuteReader())
+                    while (reader.Read())
+                        _lecturers.Add(new DbLecturer
+                        {
+                            UUID = Guid.Parse(reader["UUID"].ToString()),
+                            title_before = reader["title_before"].ToString(),
+                            first_name = reader["first_name"].ToString(),
+                            middle_name = reader["middle_name"].ToString(),
+                            last_name = reader["last_name"].ToString(),
+                            title_after = reader["title_after"].ToString(),
+                            picture_url = reader["picture_url"].ToString(),
+                            location = reader["location"].ToString(),
+                            claim = reader["claim"].ToString(),
+                            bio = reader["bio"].ToString(),
+                            tags = reader["tags"].ToString(),
+                            price_per_hour = Convert.ToUInt32(reader["price_per_hour"]),
+                            contact = reader["contact"].ToString(),
+                        });
+            });
+
+            lectuerers.Clear();
+            for (int i = 0; i < _lecturers.Count; i++)
+                lectuerers.Add(_lecturers[i].UUID, _lecturers[i]);
+
+            tags.Clear();
+            runCommand(command =>
+            {
+                command.CommandText = "SELECT * FROM Tags;";
+
+                using (SqliteDataReader reader = command.ExecuteReader())
+                    while (reader.Read())
+                        tags.Add(new Lecturer.Tag
+                        {
+                            uuid = Guid.Parse(reader["UUID"].ToString()),
+                            name = reader["name"].ToString(),
+                        });
+            });
+        }
 
         private static SqliteConnection createConenction()
         {
@@ -31,23 +109,82 @@ namespace aspnetapp
             connection.Dispose();
         }
 
-        public static void AddLectuer(Lecturer lecturer)
-            => lectuerers.Add(lecturer.UUID, lecturer);
+        public static void AddLectuer(DbLecturer lecturer)
+        {
+            runCommand(command =>
+            {
+                command.CommandText = @"
+                    INSERT INTO Lecturers (
+                        UUID, title_before, first_name, middle_name, last_name,
+                        title_after, picture_url, location, claim, bio, tags,
+                        price_per_hour, contact
+                    ) VALUES (
+                        @UUID, @title_before, @first_name, @middle_name, @last_name,
+                        @title_after, @picture_url, @location, @claim, @bio, @tags,
+                        @price_per_hour, @contact
+                    );
+                ";
 
-        public static Lecturer GetLecturer(Guid uuid)
+                command.Parameters.AddWithValue("@UUID", lecturer.UUID.ToString());
+                command.Parameters.AddWithValue("@title_before", lecturer.title_before);
+                command.Parameters.AddWithValue("@first_name", lecturer.first_name);
+                command.Parameters.AddWithValue("@middle_name", lecturer.middle_name);
+                command.Parameters.AddWithValue("@last_name", lecturer.last_name);
+                command.Parameters.AddWithValue("@title_after", lecturer.title_after);
+                command.Parameters.AddWithValue("@picture_url", lecturer.picture_url);
+                command.Parameters.AddWithValue("@location", lecturer.location);
+                command.Parameters.AddWithValue("@claim", lecturer.claim);
+                command.Parameters.AddWithValue("@bio", lecturer.bio);
+                command.Parameters.AddWithValue("@tags", lecturer.tags);
+                command.Parameters.AddWithValue("@price_per_hour", lecturer.price_per_hour);
+                command.Parameters.AddWithValue("@contact", lecturer.contact);
+
+                command.ExecuteNonQuery();
+            });
+            lectuerers.Add(lecturer.UUID, lecturer);
+        }
+
+        public static DbLecturer GetLecturer(Guid uuid)
             => lectuerers[uuid];
 
-        public static bool TryGetLecturer(Guid uuid, out Lecturer lecturer)
+        public static bool TryGetLecturer(Guid uuid, out DbLecturer lecturer)
             => lectuerers.TryGetValue(uuid, out lecturer);
 
         public static bool ContainsKey(Guid uuid)
             => lectuerers.ContainsKey(uuid);
 
         public static bool Remove(Guid uuid)
-            => lectuerers.Remove(uuid);
+        {
+            runCommand(command =>
+            {
+                command.CommandText = "DELETE FROM Lecturers WHERE UUID = @UUID;";
+                command.Parameters.AddWithValue("@UUID", uuid.ToString());
+
+                command.ExecuteNonQuery();
+            });
+            return lectuerers.Remove(uuid);
+        }
 
         public static void AddTag(Lecturer.Tag tag)
-            => tags.Add(tag);
+        {
+            runCommand(command =>
+            {
+                command.CommandText = @"
+                    INSERT INTO Tags (
+                        UUID, name
+                    ) VALUES (
+                        @UUID, @name
+                    );
+                ";
+
+                command.Parameters.AddWithValue("@UUID", tag.uuid.ToString());
+                command.Parameters.AddWithValue("@name", tag.name);
+
+                command.ExecuteNonQuery();
+            });
+
+            tags.Add(tag);
+        }
 
         public static bool ContainsTag(Lecturer.Tag tag)
             => tags.Contains(tag);
---
+++ aspnetapp/Models/DbLecturer.cs
@@ -0,0 +1,98 @@
+﻿using System.Text;
+
+namespace aspnetapp.Models
+{
+    public class DbLecturer
+    {
+        public Guid UUID { get; set; }
+        public string? title_before { get; set; }
+        public string first_name { get; set; }
+        public string? middle_name { get; set; }
+        public string last_name { get; set; }
+        public string? title_after { get; set; }
+        public string? picture_url { get; set; }
+        public string? location { get; set; }
+        public string? claim { get; set; }
+        public string? bio { get; set; }
+        public string tags { get; set; }
+        public uint? price_per_hour { get; set; }
+        public string contact { get; set; }
+
+        public static implicit operator DbLecturer(Lecturer lecturer)
+            => new DbLecturer()
+            {
+                UUID = lecturer.UUID,
+                title_before = lecturer.title_before,
+                first_name = lecturer.first_name,
+                middle_name = lecturer.middle_name,
+                last_name = lecturer.last_name,
+                title_after = lecturer.title_after,
+                picture_url = lecturer.picture_url,
+                location = lecturer.location,
+                claim = lecturer.claim,
+                bio = lecturer.bio,
+                tags = TagsToString(lecturer.tags),
+                price_per_hour = lecturer.price_per_hour,
+                contact = ContactToString(lecturer.contact),
+            };
+        public static implicit operator Lecturer(DbLecturer lecturer)
+            => new Lecturer()
+            {
+                UUID = lecturer.UUID,
+                title_before = lecturer.title_before,
+                first_name = lecturer.first_name,
+                middle_name = lecturer.middle_name,
+                last_name = lecturer.last_name,
+                title_after = lecturer.title_after,
+                picture_url = lecturer.picture_url,
+                location = lecturer.location,
+                claim = lecturer.claim,
+                bio = lecturer.bio,
+                tags = StringToTags(lecturer.tags),
+                price_per_hour = lecturer.price_per_hour,
+                contact = StringToContact(lecturer.contact),
+            };
+
+        private static string TagsToString(Lecturer.Tag[] tags)
+        {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < tags.Length; i++)
+                builder.Append(tags[i].uuid + ":" + tags[i].name + ";");
+            return builder.ToString();
+        }
+        private static string ContactToString(Lecturer.Contact contact)
+        {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < contact.telephone_numbers.Length; i++)
+                builder.Append(contact.telephone_numbers[i] + ",");
+            builder.Append(";");
+            for (int i = 0; i < contact.emails.Length; i++)
+                builder.Append(contact.emails[i] + ",");
+            return builder.ToString();
+        }
+        private static Lecturer.Tag[] StringToTags(string s)
+        {
+            string[] split = s.Split(';', StringSplitOptions.RemoveEmptyEntries);
+            Lecturer.Tag[] tags = new Lecturer.Tag[split.Length];
+            for (int i = 0; i < split.Length; i++)
+            {
+                string[] split2 = split[i].Split(':');
+                tags[i] = new Lecturer.Tag()
+                {
+                    uuid = new Guid(split2[0]),
+                    name = split2[1]
+                };
+            }
+            return tags;
+        }
+        private static Lecturer.Contact StringToContact(string s)
+        {
+            string[] split = s.Split(';');
+            return new Lecturer.Contact()
+            {
+                telephone_numbers = split[0].Split(',', StringSplitOptions.RemoveEmptyEntries),
+                emails = split[1].Split(',', StringSplitOptions.RemoveEmptyEntries),
+            };
+        }
+    }
+}
