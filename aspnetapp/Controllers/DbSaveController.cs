d5f4ef30e70ed527540df215b5cfe294ef7fc0f1
SuperMatejCZ <mata.v114@gmail.com>
2023-12-11 14:03:17 +0100

    more fix

--- aspnetapp/Controllers/DbSaveController.cs
+++ aspnetapp/Controllers/DbSaveController.cs
@@ -13,7 +13,23 @@ namespace aspnetapp.Controllers
             try
             {
                 Database.Save();
+                return Ok();
             } catch(Exception ex)
+            {
+                Log.Exception(ex);
+                return StatusCode(500);
+            }
+        }
+        [HttpGet]
+        [Route("load")]
+        public ActionResult Load()
+        {
+            try
+            {
+                Database.Load();
+                return Ok();
+            }
+            catch (Exception ex)
             {
                 Log.Exception(ex);
                 return StatusCode(500);
