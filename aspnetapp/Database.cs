bbfac43aeed19a747798cf051c9a7cf96d95753a
SuperMatejCZ <mata.v114@gmail.com>
2023-12-11 13:32:02 +0100

    DELETED database

--- aspnetapp/Database.cs
+++ aspnetapp/Database.cs
@@ -8,6 +8,30 @@ namespace aspnetapp
 {
     public static class Database
     {
+        public static readonly Dictionary<Guid, Lecturer> lectuerers = new Dictionary<Guid, Lecturer>();
+        public static readonly List<Lecturer.Tag> tags = new List<Lecturer.Tag>();
+
+        public static void AddLectuer(Lecturer lecturer)
+            => lectuerers.Add(lecturer.UUID, lecturer);
+
+        public static Lecturer GetLecturer(Guid uuid)
+            => lectuerers[uuid];
+
+        public static bool TryGetLecturer(Guid uuid, out Lecturer lecturer)
+            => lectuerers.TryGetValue(uuid, out lecturer);
+
+        public static bool ContainsKey(Guid uuid)
+            => lectuerers.ContainsKey(uuid);
+
+        public static bool Remove(Guid uuid)
+            => lectuerers.Remove(uuid);
+
+        public static void AddTag(Lecturer.Tag tag)
+            => tags.Add(tag);
+
+        public static bool ContainsTag(Lecturer.Tag tag)
+            => tags.Contains(tag);
+        /*
         private static readonly string SavePath = Path.Combine(Environment.CurrentDirectory, "database.save");
 
         public static readonly Dictionary<Guid, DbLecturer> lectuerers = new Dictionary<Guid, DbLecturer>();
@@ -144,7 +168,7 @@ namespace aspnetapp
 
         public static bool ContainsTag(Lecturer.Tag tag)
             => tags.Contains(tag);
-
+        */
         /*public static void Init()
         {
             runCommand(command =>
