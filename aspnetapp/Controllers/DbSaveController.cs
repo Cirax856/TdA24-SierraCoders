e08efead8532fafeef85cedc3b3a6c87caeea74f
SuperMatejCZ <mata.v114@gmail.com>
2023-12-11 14:01:23 +0100

    Fixed db for the 1000th time

---
+++ aspnetapp/Controllers/DbSaveController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace aspnetapp.Controllers
+{
+    [ApiController]
+    [Route("plspls")]
+    public class DbSaveController : Controller
+    {
+        [HttpGet]
+        [Route("save")]
+        public ActionResult Save()
+        {
+            try
+            {
+                Database.Save();
+            } catch(Exception ex)
+            {
+                Log.Exception(ex);
+                return StatusCode(500);
+            }
+        }
+    }
+}
