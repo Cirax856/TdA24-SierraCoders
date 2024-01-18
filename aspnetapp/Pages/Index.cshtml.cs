ea49e376d4b001129514613046f300d66da05eba
BitcoderCZ <mata.v114@gmail.com>
2024-01-18 18:44:57 +0100

    Added filtering by tags

--- aspnetapp/Pages/Index.cshtml.cs
+++ aspnetapp/Pages/Index.cshtml.cs
@@ -10,7 +10,7 @@ public class IndexModel : PageModel
     private readonly ILogger<IndexModel> _logger;
 
     public KeyValuePair<Guid, DbLecturer>[] lecturers { get; private set; }
-    public Searcher.RatedString[] result { get; private set; }
+    public List<Searcher.RatedString> result { get; private set; }
 
     public IndexModel(ILogger<IndexModel> logger)
     {
@@ -24,17 +24,38 @@ public class IndexModel : PageModel
             searchQuery = _searchQuery.ToString().ToLowerInvariant();
 
         lecturers = Database.lectuerers.ToArray();
-        result = Searcher.Search(Database.lectuerers.ToArray(), lecturer => ((Lecturer)lecturer.Value).DisplayName.ToLowerInvariant(), searchQuery).ToArray();
+        result = Searcher.Search(Database.lectuerers.ToArray(), lecturer => ((Lecturer)lecturer.Value).DisplayName.ToLowerInvariant(), searchQuery).ToList();
 
-        if (result.Length == 0)
+        if (result.Count == 0)
             return;
 
         // remove entries with score lower than or equal to 0
         int i;
-        for (i = 0; i < result.Length; i++)
+        for (i = 0; i < result.Count; i++)
             if (result[i].Score <= 0f)
                 break;
 
-        result = result.Take(i).ToArray();
+        result = result.Take(i).ToList();
+
+        string[] tags = new string[0];
+        if (Request.Query.TryGetValue("tag", out StringValues _tags))
+            tags = _tags.ToArray();
+
+        for (i = 0; i < result.Count; i++)
+        {
+            bool invalid = false;
+            for (int j = 0; j < tags.Length; j++)
+                if (!lecturers[result[i].OgIndex].Value.tags.ToLowerInvariant().Contains(tags[j].ToLowerInvariant()))
+                {
+                    invalid = true;
+                    break;
+                }
+
+            if (invalid)
+            {
+                result.RemoveAt(i);
+                i--;
+            }
+        }
     }
 }
