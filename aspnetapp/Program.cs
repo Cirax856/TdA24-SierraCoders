e3112ebf9924b5cd20f949de485dc67b795b409f
Cirax856 <Michal.Schneider07@seznam.cz>
2024-01-18 18:27:06 +0100

    Removed test lecturers and changed title

--- aspnetapp/Program.cs
+++ aspnetapp/Program.cs
@@ -16,59 +16,6 @@ namespace aspnetapp {
                 Log.Exception(ex);
             }
 
-            Database.AddLectuer(new Lecturer()
-            {
-                UUID = Guid.NewGuid(),
-                first_name = "AA",
-                last_name = "BB",
-                location = "C",
-                picture_url = "https://th.bing.com/th/id/OIP.tITS7zP_lmwIVB21WfF9WgAAAA?rs=1&pid=ImgDetMain",
-                tags = new Lecturer.Tag[]
-                {
-                    new Lecturer.Tag()
-                    {
-                        uuid = Guid.NewGuid(),
-                        name = "tag"
-                    },
-                    new Lecturer.Tag()
-                    {
-                        uuid = Guid.NewGuid(),
-                        name = "cooltag"
-                    }
-                },
-                contact = new Lecturer.Contact()
-                {
-                    emails = new string[0],
-                    telephone_numbers = new string[0]
-                }
-            }) ;
-            Database.AddLectuer(new Lecturer()
-            {
-                UUID = Guid.NewGuid(),
-                first_name = "CC",
-                last_name = "DD",
-                location = "E",
-                picture_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Wikipe-tan_full_length.png/330px-Wikipe-tan_full_length.png",
-                tags = new Lecturer.Tag[]
-               {
-                    new Lecturer.Tag()
-                    {
-                        uuid = Guid.NewGuid(),
-                        name = "tag"
-                    },
-                    new Lecturer.Tag()
-                    {
-                        uuid = Guid.NewGuid(),
-                        name = "cooltag"
-                    }
-               },
-                contact = new Lecturer.Contact()
-                {
-                    emails = new string[0],
-                    telephone_numbers = new string[0]
-                }
-            });
-
             /*List<string> inputs = new List<string>();
             
             do
