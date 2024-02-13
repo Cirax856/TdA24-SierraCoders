d9919b5ef41b8ede0d40094c6c02b29feaa1c5d4
SuperMatejCZ <mata.v114@gmail.com>
2024-02-13 12:06:14 +0100

    Started adding authorization

---
+++ aspnetapp/Auth/LoggedInPolicyProvider.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace aspnetapp.Auth
+{
+    public class LoggedInPolicyProvider : IAuthorizationPolicyProvider
+    {
+        const string POLICY_PREFIX = "LoggedIn";
+        public DefaultAuthorizationPolicyProvider FallbackPolicyProvider { get; }
+        public LoggedInPolicyProvider(IOptions<AuthorizationOptions> options)
+        {
+            FallbackPolicyProvider = new DefaultAuthorizationPolicyProvider(options);
+        }
+
+        public Task<AuthorizationPolicy> GetDefaultPolicyAsync() =>
+                                FallbackPolicyProvider.GetDefaultPolicyAsync();
+        public Task<AuthorizationPolicy?> GetFallbackPolicyAsync() =>
+                                FallbackPolicyProvider.GetFallbackPolicyAsync();
+
+        public Task<AuthorizationPolicy?> GetPolicyAsync(string policyName)
+        {
+            if (policyName.StartsWith(POLICY_PREFIX, StringComparison.OrdinalIgnoreCase))
+            {
+                AuthorizationPolicyBuilder policy = new AuthorizationPolicyBuilder(JwtBearerDefaults.AuthenticationScheme);
+                policy.AddRequirements(new LoggedInRequirement());
+
+                return Task.FromResult<AuthorizationPolicy?>(policy.Build());
+            }
+
+            return Task.FromResult<AuthorizationPolicy?>(null);
+        }
+    }
+}
