4ca18882eac5addae960d35d528eaf5489bf6b8e
SuperMatejCZ <mata.v114@gmail.com>
2024-02-16 12:52:53 +0100

    Index -> lecturers

---
+++ aspnetapp/Controllers/AcountController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspnetapp.Controllers
+{
+    [Route("api/account")]
+    [ApiController]
+    public class AcountController : ControllerBase
+    {
+    }
+}
